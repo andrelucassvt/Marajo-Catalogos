dbfe6410cf129116bd7b9bd7f2f4163b36e9bb53
andrelucassvt <andrelucassvt99@gmail.com>
2020-05-02 04:02:23 -0300

    Imagem adicionada

--- lib/main.dart
+++ lib/main.dart
@@ -20,24 +20,32 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Marajó catálogo",style: TextStyle(color: Colors.white),),
         backgroundColor: Colors.orange,
       ),
+      
+      
+      body: Stack(children: <Widget>[
 
-      body:Column(
-        
+      new Image.asset('Imagens/Marajoara.jpg',fit: BoxFit.fill,height: 1000.0,),
+
+      //Texto inicial
+      Padding(padding: EdgeInsets.all(10.0),
+      child:Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
 
-          Text("Salvaterra",
-          style: TextStyle(color: Colors.black,fontSize: 25.0),)
+          Text("Bem vindo ao catálago marajoara.",
+          style: TextStyle(color: Colors.white70,fontSize: 25.0,backgroundColor:Colors.black45,))
          
-        ],),
+        ],), 
+      ),
       
-     
-    
+
+      ],),
+
       //Botao
       floatingActionButton: SpeedDial(
         animatedIcon: AnimatedIcons.menu_close,
@@ -71,20 +79,13 @@ class _HomeState extends State<Home> {
                     fontWeight: FontWeight.w500,
                     color: Colors.white,
                     fontSize: 16.0),
-                labelBackgroundColor: Colors.blue)
+                labelBackgroundColor: Colors.blue),
+
           ],
       ),
 
-      //Barra
-       bottomNavigationBar: BottomAppBar(
-      shape: const CircularNotchedRectangle(),
-      color: Colors.orange,
-      child: Container(height: 50.0,),
-      ),
-
-
-      //Botao no meio de navigation
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      
     );
+    
   }
 }
