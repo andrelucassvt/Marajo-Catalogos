d556a60055870747a298fe0b5e9b9da5d24b0c92
andrelucassvt <andrelucassvt99@gmail.com>
2020-05-03 14:56:21 -0300

    Adicionado restaurantes testes

--- lib/telas/salvaterra.dart
+++ lib/telas/salvaterra.dart
@@ -12,12 +12,79 @@ class salvaterraHome extends StatelessWidget {
         backgroundColor: Colors.orange,
       ),
 
-      body: Column(children: <Widget>[
+      body: ListView(children: <Widget>[
+        //LOJA 1 TESTE
+        Card(
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage("Imagens/umuarama.jpg"))
+                ),
+                child: InkWell(
+                  onTap: (){},
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Column(children: <Widget>[
+                  Text("Pousada Umuarama",style: TextStyle(color: Colors.black,fontSize: 22.0),),
+                  Center(
+                    child:
+                     Text("Clique para mais informações",
+                     style: TextStyle(color: Colors.black,fontSize: 14.0),),
+                     )
+                ],),
+                )
+              ],
+            ),
+            ),
+        ),
+        //FIM LOJA TESTE
 
-     
+        //Loja teste 2
+        Card(
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage("Imagens/sabormarajo.jpg"))
+                ),
+                child: InkWell(
+                  onTap: (){},
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Column(children: <Widget>[
+                  Text("Sabor marajó",style: TextStyle(color: Colors.black,fontSize: 22.0),),
+                  Center(
+                    child:
+                     Text("Clique para mais informações",
+                     style: TextStyle(color: Colors.black,fontSize: 14.0),),
+                     )
+                ],),
+                )
+              ],
+            ),
+            ),
+        ),
+        //FIM loja teste 2
+
+
 
       ],)
-
+      
 
     );
   }
